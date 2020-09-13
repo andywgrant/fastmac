0e78545b0bdaf65dcacb9351a56de20e586c8643
andywgrant <andywgrant@users.noreply.github.com>
2020-09-13 16:40:38 -0700

    Add files via upload

---
+++ script-mac-gui.sh
@@ -0,0 +1,11 @@
+# disable spotlight indexing
+sudo mdutil -i off -a
+echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+sudo /usr/bin/dscl . -passwd /Users/runner runner123
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers
+# Reverse tunnel to screen share port
+# You’ll need to create a free https://ngrok.com/ account to get an auth token (needed for tcp tunnel and to remotely get the endpoint of the tunnel created) and replace “<ngrok_auth_token>” with this value.
+mkdir /tmp/gui
+curl -o /tmp/gui/z.$$ https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip && (cd /tmp/gui && unzip /tmp/gui/z.$$) && rm /tmp/gui/z.$$
+/tmp/gui/ngrok authtoken 1hTflrwncelU7Uexv9bmSYiHKOl_54pPSvNe5XxEydJm1uHD4 --config /tmp/gui/ngrok.yml
+/tmp/gui/ngrok tcp 5900 -log=stdout --config /tmp/gui/ngrok.yml > /dev/null &
