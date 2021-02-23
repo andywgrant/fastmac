279467775a305d858c2fd16b3139e16b03c99b0d
andywgrant <andywgrant@users.noreply.github.com>
2021-02-23 01:02:36 -0800

    Update script-mac-gui.sh

--- script-mac-gui.sh
+++ script-mac-gui.sh
@@ -10,7 +10,7 @@ sudo /usr/bin/dscl . -passwd /Users/guiadmin gui-runner123
 sudo /usr/bin/dscl . -append /Groups/admin GroupMembership guiadmin
 sudo mkdir /Users/guiadmin
 sudo chown -R 1013 /Users/guiadmin
-sudo su -l guidadmin
+sudo su -l guiadmin
 exit
 sudo -u guiadmin defaults write com.apple.SetupAssistant.managed SkipAppearance -bool true
 sudo -u guiadmin defaults write com.apple.SetupAssistant.managed SkipCloudSetup -bool true
